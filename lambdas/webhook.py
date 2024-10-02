8bf282ed1d072ee06355d8ed13537919494b3030
Kelly Haro Vasconcellos <kelly.vasconcellos@bazardoconsorcio.com.br>
2024-10-02 17:16:21 -0300

    feat: unindo funcoes de script externo

--- lambdas/webhook.py
+++ lambdas/webhook.py
@@ -2,6 +2,7 @@ import json
 import logging
 import requests
 import os
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 # Criação do logger e definição do nível de log
@@ -32,54 +33,84 @@ def lambda_handler(event, context):
     
 def processar_webhook_resposta(body):
     resposta = json.loads(body)
-
-    # Cenário de Sucesso
     validacao_data = resposta['data']
 
     if validacao_data.get('valid') is True:
-        dados_validos = 'VALIDO',
+        return processar_sucesso(validacao_data)
+    else:
+        return processar_erro(validacao_data)
+    
+
+def processar_sucesso(validacao_data):
+    dados_validos = 'VALIDO'
+
+    # Obter a URL pré-assinada
+    url_pre_assinada = obter_url_pre_assinada("301247725", "comprovante_microdeposito.pdf")
+    logger.info(f"URL pré-assinada obtida: {url_pre_assinada}")
+
+    # Extrair o caminho formatado da URL
+    caminho_formatado = extrair_caminho(url_pre_assinada)
+    logger.info(f"Caminho formatado: {caminho_formatado}")
+
+    # Fazer o upload do arquivo
+    arquivo_url = validacao_data.get('receipt_url')
+    logger.info(f"Validacao data: {validacao_data}")
+    logger.info(f"Receipt URL: {arquivo_url}")
+
+    sucesso_upload = fazer_upload(url_pre_assinada, arquivo_url)
+    if sucesso_upload:
+        logger.info("Upload realizado com sucesso.")
 
         # Atualizar campos no Pipefy
-        atualizar_campos_pipefy(
+        resposta_atualizacao = atualizar_campos_pipefy(
             node_id="971537587",
             dados_validos=dados_validos,
-            erro_dados_bancarios=""
+            erro_dados_bancarios="",
+            caminho_arquivo=caminho_formatado  # Adiciona o caminho do comprovante
         )
+        logger.info(f"Resposta da atualização: {resposta_atualizacao}")
 
+        # Verifique se o comprovante precisa ser atualizado
+        if dados_validos and caminho_formatado:
+            atualizar_comprovante(node_id="971537587", caminho_arquivo=caminho_formatado)
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps({
-                'message': 'Conta validada com sucesso',
-                'account_info': {
-                    'agencia': validacao_data['data'].get('agency', 'AGENCIA_DESCONHECIDA'),
-                    'conta': validacao_data['data'].get('account', 'CONTA_DESCONHECIDA'),
-                    'cpf_cnpj': validacao_data['data'].get('cpf_cnpj', 'CPF_CNPJ_DESCONHECIDO'),
-                    'tipo_conta': validacao_data['data'].get('account_type', 'TIPO_CONTA_DESCONHECIDA'),
-                    'digito_conta': validacao_data['data'].get('account_digit', 'DIGITO_DESCONHECIDO')
-                },
-            }, ensure_ascii=False)
-        }
-    
     else:
-        # Cenário de erro
-        erros = validacao_data.get('errors', [])
-        erros_formatados = formatar_erros(erros)
+        logger.error("Falha ao realizar o upload do arquivo.")
 
-        # Atualizar campos no Pipefy com erro
-        atualizar_campos_pipefy(
-            node_id="971537587",
-            dados_validos="INVALIDO",
-            erro_dados_bancarios=erros_formatados
-        )
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Conta validada com sucesso',
+            'account_info': {
+                'agencia': validacao_data.get('agency', 'AGENCIA_DESCONHECIDA'),
+                'conta': validacao_data.get('account', 'CONTA_DESCONHECIDA'),
+                'cpf_cnpj': validacao_data.get('cpf_cnpj', 'CPF_CNPJ_DESCONHECIDO'),
+                'tipo_conta': validacao_data.get('account_type', 'TIPO_CONTA_DESCONHECIDA'),
+                'digito_conta': validacao_data.get('account_digit', 'DIGITO_DESCONHECIDO'),
+                "receipt_url": validacao_data.get('receipt_url', 'URL_DESCONHECIDA')
+            },
+        }, ensure_ascii=False)
+    }
 
-        return {
-            'statusCode': 400,
-            'body': json.dumps({
-                'message': 'Dados inválidos',
-                'errors': erros_formatados
-            }, ensure_ascii=False)
-        }
+def processar_erro(validacao_data):
+    erros = validacao_data.get('errors', [])
+    erros_formatados = formatar_erros(erros)
+
+    # Atualizar campos no Pipefy com erro
+    atualizar_campos_pipefy(
+        node_id="971537587",
+        dados_validos="INVALIDO",
+        erro_dados_bancarios=erros_formatados
+    )
+
+    return {
+        'statusCode': 400,
+        'body': json.dumps({
+            'message': 'Dados inválidos',
+            'errors': erros_formatados
+        }, ensure_ascii=False)
+    }
+
 
 def formatar_erros(erros):
     mapa_campos = {
@@ -103,7 +134,56 @@ def formatar_erros(erros):
 
     return '. '.join(mensagens_formatadas)
 
-def atualizar_campos_pipefy(node_id, dados_validos, erro_dados_bancarios=None):
+
+def obter_url_pre_assinada(id_organizacao, nome_arquivo):
+    url = 'https://api.pipefy.com/graphql'
+    headers = {
+        "Authorization": f"Bearer {PIPEFY_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    query = f"""
+    mutation {{
+        createPresignedUrl(input: {{ organizationId: {id_organizacao}, fileName: "{nome_arquivo}" }}) {{
+            url
+        }}
+    }}
+    """
+    response = requests.post(url, headers=headers, json={'query': query})
+    return response.json()['data']['createPresignedUrl']['url']
+
+
+def fazer_upload(url_pre_assinada, arquivo):
+    headers = {
+        'Content-Type': 'application/pdf'
+    }
+    response = requests.put(url_pre_assinada, headers=headers, data=requests.get(arquivo).content)
+
+    if response.status_code == 200:
+        logger.info("Upload realizado com sucesso.")
+    else:
+        logger.error(f"Falha no upload. Status Code: {response.status_code}, Resposta: {response.text}")
+
+    return response.status_code == 200
+
+
+def extrair_caminho(url_pre_assinada):
+    parsed_url = urlparse(url_pre_assinada)
+    caminho = parsed_url.path.lstrip('/')
+
+    # Adicionando "orgs/" ao caminho
+    partes = caminho.split('/')
+    if len(partes) >= 5:
+        return "/".join(partes[0:2]) + "/" + "/".join(partes[2:])  # Inclui "orgs/" e o restante
+    return caminho  # Se não tiver partes suficientes, retorna o caminho completo
+
+
+def atualizar_campos_pipefy(node_id, dados_validos, erro_dados_bancarios=None, caminho_arquivo=None):
+    url = 'https://api.pipefy.com/graphql'
+    headers = {
+        "Authorization": f"Bearer {PIPEFY_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
     mutation = """
     mutation ($nodeId: ID!, $values: [NodeFieldValueInput!]!) {
         updateFieldsValues(input: {
@@ -114,10 +194,7 @@ def atualizar_campos_pipefy(node_id, dados_validos, erro_dados_bancarios=None):
         }
     }
     """
-
-    values = [
-        {'fieldId': "dados_v_lidos", 'value': dados_validos},
-    ]
+    values = [{'fieldId': "dados_v_lidos", 'value': dados_validos}]  # Use a variável diretamente
 
     if erro_dados_bancarios is not None:
         values.append({'fieldId': "erro_dados_bancarios", 'value': erro_dados_bancarios})
@@ -127,20 +204,49 @@ def atualizar_campos_pipefy(node_id, dados_validos, erro_dados_bancarios=None):
         'values': values
     }
 
-    data = {'query': mutation, 'variables': variables}
+    logger.info("Enviando mutação para o Pipefy: %s", variables)
+
+    response = requests.post(url, json={'query': mutation, 'variables': variables}, headers=headers)
+
+    if response.status_code == 200:
+        logger.info("Campos atualizados com sucesso: %s", response.json())
+
+        # Atualiza o campo de comprovante se necessário
+        if dados_validos and caminho_arquivo:
+            mutation_comprovante = f"""
+            mutation {{
+                updateCardField(input: {{ card_id: {node_id}, field_id: "comprovante_microdeposito", new_value: ["{caminho_arquivo}"] }}) {{
+                    clientMutationId
+                    success
+                }}
+            }}
+            """
+            response_comprovante = requests.post(url, headers=headers, json={'query': mutation_comprovante})
+            logger.info("Comprovante atualizado: %s", response_comprovante.json())
+            return response_comprovante.json()
+
+    else:
+        logger.error("Erro ao atualizar os campos: %s - %s", response.status_code, response.text)
+    return response.json()
+
+
+def atualizar_comprovante(node_id, caminho_arquivo):
+    url = 'https://api.pipefy.com/graphql'
     headers = {
         "Authorization": f"Bearer {PIPEFY_TOKEN}",
         "Content-Type": "application/json"
     }
-
-    logger.info("Enviando mutação para o Pipefy: %s", data)
-
-    response = requests.post(URL_PIPEFY, json=data, headers=headers)
-
-    if response.status_code == 200:
-        logger.info("Campos atualizados com sucesso: %s", response.json())
-    else:
-        logger.error("Erro ao atualizar os campos: %s - %s", response.status_code, response.text)
+    mutation = f"""
+    mutation {{
+        updateCardField(input: {{ card_id: {node_id}, field_id: "comprovante_microdeposito", new_value: ["{caminho_arquivo}"] }}) {{
+            clientMutationId
+            success
+        }}
+    }}
+    """
+    response = requests.post(url, headers=headers, json={'query': mutation})
+    logger.info("Comprovante atualizado: %s", response.json())
+    return response.json()
 
 
 if __name__ == "__main__":
